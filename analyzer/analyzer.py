84d9023b99e23d711576b9792bf977dfac60af70
Jan Peřina <perinja2@gmail.com>
2023-05-02 22:07:15 +0200

    make analyzer save processed files

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -2,64 +2,94 @@ import networkx as nx
 import json
 from hashlib import sha1
 from collections import defaultdict
-from typing import Iterator
+from typing import Iterable
 import numpy as np
 from analyzer.model import Model
 from typing import Any
 import pandas as pd
 import seaborn as sns
+import tqdm
+
 
 class TraceAnalyzer:
-
-    def __init__(self, file_iter):
-        self.G : nx.Graph = nx.DiGraph()
-        self.file_iter: Iterator[str] = file_iter
+    def __init__(self, files):
+        self.G: nx.Graph = nx.DiGraph()
+        self.files: Iterable[str] = files
         self.hash_trace: dict[str, tuple[str, str, list[str]]] = {}
         self.models: dict[tuple[str, str], Model] = {}
         self.hash_counter: dict[str, int] = defaultdict(lambda: 0)
-        self.markov_probs: dict[str, dict[str, float]] = defaultdict(lambda: defaultdict(lambda: 1))
-        self.src_dest_freq: dict[str, dict[str, int]] = defaultdict(lambda: defaultdict(lambda: 0))
+        self.markov_probs: dict[str, dict[str, float]] = defaultdict(
+            lambda: defaultdict(lambda: 1)
+        )
+        self.src_dest_freq: dict[str, dict[str, int]] = defaultdict(
+            lambda: defaultdict(lambda: 0)
+        )
         self.sources: dict[str, int] = defaultdict(lambda: 0)
         self.destinations: dict[str, int] = defaultdict(lambda: 0)
-        self.scores: dict[str, dict[int, Any]] = defaultdict(lambda:{})
+        self.scores: dict[str, dict[int, Any]] = defaultdict(lambda: {})
         self.data = []
         self.n = 0
 
     @staticmethod
     def hash_route(src, dest, hops) -> str:
-        return sha1((f"{src}{' '.join(hops)}{dest}").encode('ascii')).hexdigest()
+        return sha1((f"{src}{' '.join(hops)}{dest}").encode("ascii")).hexdigest()
 
-    def process(self):
-        for file in self.file_iter:
+    def process(self, progressbar=None):
+        files: Iterable[str] = tqdm.tqdm(self.files) if progressbar else self.files
+
+        for file in files:
             with open(file, "r") as f:
                 json_data = json.load(f)
             self.process_traceroute(json_data)
 
+    def process_and_save(self, folder, progressbar=None):
+        files = tqdm.tqdm(self.files) if progressbar else self.files
+
+        for file in files:
+            with open(file, "r") as f:
+                json_data = json.load(f)
+            score = self.process_traceroute(json_data)
+
+            # rtt_is_outlier, rtt_prob, rtt_mu_diff, success_prob, success_score, rtt_ttl_rate
+            json_data["outlier"] = score[0].tolist()
+            json_data["rtts_prob"] = score[1].tolist()
+            json_data["rtt_error"] = score[2].tolist()
+            json_data["success_prob"] = score[3].tolist()
+            json_data["success_score"] = score[4].tolist()
+            json_data["rtt_ttl_rate"] = score[5].tolist()
+
+            with open(f"{folder}/{file.split('/')[-1]}", "w") as f:
+                json.dump(json_data, f)
+
     def get_edge_model(self, u, v) -> Model:
         self.markov_probs[u][v] += 1
         if (u, v) not in self.models:
-                    self.G.add_edge(u, v)
-                    self.models[(u, v)] = Model(u, v)
-        return self.models[(u, v)] 
-    
+            self.G.add_edge(u, v)
+            self.models[(u, v)] = Model(u, v)
+        return self.models[(u, v)]
+
     def get_probs(self, node, scale=False):
-         node_out = self.markov_probs[node]
-         n = sum(node_out.values())
-         probs = {x: node_out[x]/n for x in node_out}
-         if scale:
-            frac = max(probs.values())-min(probs.values())
-            probs = {k:v/frac for k,v in probs.items()}
-         return probs
-    
+        node_out = self.markov_probs[node]
+        n = sum(node_out.values())
+        probs = {x: node_out[x] / n for x in node_out}
+        if scale:
+            frac = max(probs.values()) - min(probs.values())
+            probs = {k: v / frac for k, v in probs.items()}
+        return probs
+
     def graph_probs(self, scale=False):
-        return {(x,y):p for x in self.markov_probs for y,p in self.get_probs(x, scale=scale).items()}
+        return {
+            (x, y): p
+            for x in self.markov_probs
+            for y, p in self.get_probs(x, scale=scale).items()
+        }
 
     def process_traceroute(self, data):
         self.n += 1
         src = data["src"]
         dest = data["dest"]
-        hops = data["hops"]    
-        ts = data['timestamp']
+        hops = data["hops"]
+        ts = data["timestamp"]
 
         self.src_dest_freq[src][dest] += 1
         self.sources[src] += 1
@@ -68,17 +98,21 @@ class TraceAnalyzer:
         route_hash = self.hash_route(src, dest, hops)
         self.hash_counter[route_hash] += 1
         self.hash_trace[route_hash] = (src, dest, hops)
-        
+
         # hop_gen = iter(hops)
         # ttl_gen = iter(np.diff(np.array(data['ttls']), prepend=0))
         # rtt_gen = iter(np.diff(np.array(data['rtts']), prepend=0))
 
         scores = []
 
-        for node, ttl, rtt, in zip(hops, data['ttls'], data['rtts']):
+        for (
+            node,
+            ttl,
+            rtt,
+        ) in zip(hops, data["ttls"], data["rtts"]):
             model = self.get_edge_model(src, node)
-            scores.append(model.score(rtt, ttl, data['destination_reached']))
-            model.log(ts, rtt, ttl, data['destination_reached'])
+            scores.append(model.score(rtt, ttl, data["destination_reached"]))
+            model.log(ts, rtt, ttl, data["destination_reached"])
 
         self.scores[route_hash][ts] = np.array(scores).T
         # curr = src
@@ -90,9 +124,17 @@ class TraceAnalyzer:
         #         curr = next_hop
         #     except StopIteration:
         #         break
-        
-        
+        return np.array(scores).T
+
     def src_dest_hist(self):
         df = pd.DataFrame(self.src_dest_freq).fillna(0).astype(int).T
-        df = df.loc[:, (df!=0).any(axis=0) ]
-        return sns.heatmap(df, cmap="Greens", annot=False, fmt="d", linewidths=1, cbar=True, square=False)
+        df = df.loc[:, (df != 0).any(axis=0)]
+        return sns.heatmap(
+            df,
+            cmap="Greens",
+            annot=False,
+            fmt="d",
+            linewidths=1,
+            cbar=True,
+            square=False,
+        )
--- analyzer/model.py
+++ analyzer/model.py
@@ -1,4 +1,4 @@
-'''Model for a single edge in graph.'''
+"""Model for a single edge in graph."""
 
 import numpy as np
 import pandas as pd
@@ -8,14 +8,15 @@ from typing import Any
 import matplotlib.pyplot as plt
 import matplotlib.figure as figure
 
+
 class Model:
-    '''Model for a single edge in graph.
-    
+    """Model for a single edge in graph.
+
     Attributes:
         u (str): source
         v (str): destination
         ctr (int): number of measurements
-        
+
         alphas (list[float]): alpha values for inverse gamma distribution
         betas (list[float]): beta values for inverse gamma distribution
         mus (list[float]): mu values for normal distribution
@@ -26,7 +27,7 @@ class Model:
         delta_ttls (list[float]): difference in TTL
         successes (list[int]): number of successes
         failures (list[int]): number of failures
-        
+
         finished (bool): whether the last traceroute has finished
         timestamp (int): timestamp of last traceroute
         delta_rtt (float): difference in RTT of last traceroute
@@ -36,11 +37,11 @@ class Model:
         mu (float): mu value of last traceroute
         sigma (float): sigma value of last traceroute
 
-    '''
-    
+    """
+
     def __init__(self, u, v, alpha_0=1, beta_0=1, mu_0=5, sigma_0=2):
-        '''Initialize a new model.
-        
+        """Initialize a new model.
+
         Args:
             u (str): source
             v (str): destination
@@ -48,8 +49,8 @@ class Model:
             beta_0 (int, optional): initial beta value for inverse gamma distribution. Defaults to 1.
             mu_0 (int, optional): prior knowledge about expected delta rtt. Defaults to 5.
             sigma_0 (int, optional): prior knowledge about std of delta rtt. Defaults to 2.
-                
-        '''
+
+        """
         self.u: str = u
         self.v: str = v
         self.ctr: int = 0
@@ -71,8 +72,8 @@ class Model:
         self.success_vars: list[float] = [0.25]
 
     def log(self, ts, delta_rtt, delta_ttl, success) -> None:
-        '''Log a new measurement.
-        
+        """Log a new measurement.
+
         Args:
             ts (int): timestamp
             delta_rtt (float): difference in RTT
@@ -82,61 +83,64 @@ class Model:
         Raises:
             ValueError: if logged timestamps is older than last one
 
-        '''
+        """
         if self.timestamp is not None and ts < self.timestamp:
             raise ValueError(f"Timestamps must be in order (ts>{self.timestamp})")
-        
+
         self.ctr += 1
         self.timestamp = ts
         self.delta_rtt = delta_rtt
         self.delta_ttl = delta_ttl
 
-        self.alpha = self.alpha + 1/2
+        self.alpha = self.alpha + 1 / 2
         self.n += 1
 
-        self.beta = self.beta + 0.5*(delta_rtt-self.mu)**2
-        self.mu = self.mu + 1/self.n * (delta_rtt-self.mu)
-        self.sigma = np.sqrt(self.beta/(self.alpha + 1))
+        self.beta = self.beta + 0.5 * (delta_rtt - self.mu) ** 2
+        self.mu = self.mu + 1 / self.n * (delta_rtt - self.mu)
+        self.sigma = np.sqrt(self.beta / (self.alpha + 1))
         self.finished = success
-    
+
     def __repr__(self) -> str:
-        '''String representation of the model.'''
+        """String representation of the model."""
         return f"{self.u} -> {self.v} (#{self.ctr} N({self.mu:.2f}, {self.sigma:.2f}))"
 
     def to_frame(self, omit_first=True) -> pd.DataFrame:
-        '''Convert the model statistics to a pandas DataFrame.'''
-        df = pd.DataFrame({
-            "delta_rtt": self.delta_rtts,
-            "delta_ttl": self.delta_ttls,
-            "ts": self.tss,
-            "alpha": self.alphas,
-            "beta": self.betas,
-            "mu": self.mus,
-            "sigma": self.sigmas,
-            "successes": self.successes,
-            "failures": self.failures,
-            "success_prob": self.success_probs,
-            "success_var": self.success_vars,
-            # "n": self.ns
-        }, index=pd.DatetimeIndex(pd.to_datetime(self.tss, unit="ms")))
+        """Convert the model statistics to a pandas DataFrame."""
+        df = pd.DataFrame(
+            {
+                "delta_rtt": self.delta_rtts,
+                "delta_ttl": self.delta_ttls,
+                "ts": self.tss,
+                "alpha": self.alphas,
+                "beta": self.betas,
+                "mu": self.mus,
+                "sigma": self.sigmas,
+                "successes": self.successes,
+                "failures": self.failures,
+                "success_prob": self.success_probs,
+                "success_var": self.success_vars,
+                # "n": self.ns
+            },
+            index=pd.DatetimeIndex(pd.to_datetime(self.tss, unit="ms")),
+        )
 
         df["u"] = self.u
         df["v"] = self.v
         df["total"] = self.ctr
-        
+
         if omit_first:
             df = df.iloc[1:]
-            
+
         return df
 
     def plot(self, **kwargs) -> figure.Figure:
-        '''Plot the model statistics.'''
+        """Plot the model statistics."""
         # ignore type checks
-        
+
         # get data
         df = self.to_frame(omit_first=True)
-        prob_changes = (df["success_prob"].unique().shape[0] > 1)
-        ttl_changes = (df["delta_ttl"].unique().shape[0] > 1)
+        prob_changes = df["success_prob"].unique().shape[0] > 1
+        ttl_changes = df["delta_ttl"].unique().shape[0] > 1
         n_axis = sum([1, prob_changes, ttl_changes])
         # create figure with 3 rows of subplots
         fig, axs = plt.subplots(n_axis, 1, sharex=True)
@@ -145,62 +149,95 @@ class Model:
         fig.suptitle(f"Statistics {self.u} -> {self.v}")
         fig.set_size_inches(15, 9)
 
-        ax: plt.Axes = axs[0] if n_axis > 1 else axs # type: ignore
+        ax: plt.Axes = axs[0] if n_axis > 1 else axs  # type: ignore
 
         # render anomaly plot
-        mu_plus_3sigma = df["mu"] + 3*df["sigma"]
-        mu_minus_3sigma = df["mu"] - 3*df["sigma"]
+        mu_plus_3sigma = df["mu"] + 3 * df["sigma"]
+        mu_minus_3sigma = df["mu"] - 3 * df["sigma"]
 
-        ax.fill_between(df.index, mu_plus_3sigma, mu_minus_3sigma, color='green', alpha=0.1) # type: ignore
+        ax.fill_between(df.index, mu_plus_3sigma, mu_minus_3sigma, color="green", alpha=0.1)  # type: ignore
 
-        (mu_plus_3sigma + df["sigma"]).plot(ax=ax, color='purple', alpha=0.5)
-        (mu_minus_3sigma - df["sigma"]).plot(ax=ax, color='purple', label='$\\pm4\\sigma$', alpha=0.5)
+        (mu_plus_3sigma + df["sigma"]).plot(ax=ax, color="purple", alpha=0.5)
+        (mu_minus_3sigma - df["sigma"]).plot(
+            ax=ax, color="purple", label="$\\pm4\\sigma$", alpha=0.5
+        )
 
-        df['delta_rtt'].plot(ax=ax, label="$\\Delta\\mathrm{rtt}$")
+        df["delta_rtt"].plot(ax=ax, label="$\\Delta\\mathrm{rtt}$")
         df["mu"].plot(ax=ax, label="$\\mathbb{E}(rtt)$")
 
-        anomalies = df[(mu_minus_3sigma >= df["delta_rtt"]) | (df["delta_rtt"] >= mu_plus_3sigma)]
-        anomalies.plot(y="delta_rtt", ax=ax, color="red", marker='o', linestyle = 'None', label="anomaly")  
-          
-        ax.set_title(f"Anomalies on RTT ({anomalies.shape[0]}, {100*anomalies.shape[0]/df.shape[0]:.3f}%)")
-        ax.legend(loc='lower center', bbox_to_anchor=(0.5, -0.15), ncol=5, fancybox=True)  
+        anomalies = df[
+            (mu_minus_3sigma >= df["delta_rtt"]) | (df["delta_rtt"] >= mu_plus_3sigma)
+        ]
+        anomalies.plot(
+            y="delta_rtt",
+            ax=ax,
+            color="red",
+            marker="o",
+            linestyle="None",
+            label="anomaly",
+        )
+
+        ax.set_title(
+            f"Anomalies on RTT ({anomalies.shape[0]}, {100*anomalies.shape[0]/df.shape[0]:.3f}%)"
+        )
+        ax.legend(
+            loc="lower center", bbox_to_anchor=(0.5, -0.15), ncol=5, fancybox=True
+        )
         ax.set_xlabel("time")
         ax.set_ylabel("$\\Delta\\mathrm{rtt}$")
 
         if prob_changes:
             # plot probability
             ax: plt.Axes = axs[1]
-            df["success_prob"].plot(ax=ax, label="$P(\\mathrm{success})$", color='green')
-            upper_bound = df["success_prob"] + 3*df["success_var"].apply(np.sqrt)
-            lower_bound = df["success_prob"] - 3*df["success_var"].apply(np.sqrt)
-            ax.fill_between(df.index, lower_bound, upper_bound, color='gray', alpha=0.2) # type: ignore
+            df["success_prob"].plot(
+                ax=ax, label="$P(\\mathrm{success})$", color="green"
+            )
+            upper_bound = df["success_prob"] + 3 * df["success_var"].apply(np.sqrt)
+            lower_bound = df["success_prob"] - 3 * df["success_var"].apply(np.sqrt)
+            ax.fill_between(df.index, lower_bound, upper_bound, color="gray", alpha=0.2)  # type: ignore
             ax.set_ylabel("$P(\\mathrm{success})$")
             ax.set_title(f"Probability of success")
             ax.set_ylabel("$p$")
-            ax.set_ylim (max(0, lower_bound.min())-0.01, min(1, upper_bound.max())+0.01)
+            ax.set_ylim(
+                max(0, lower_bound.min()) - 0.01, min(1, upper_bound.max()) + 0.01
+            )
 
         if ttl_changes:
             # plot variance
             ax: plt.Axes = axs[-1]
-            df["delta_ttl"].plot(kind='line', ax=ax, label="$\\Delta(\\mathrm{ttl})$", color='red')
+            df["delta_ttl"].plot(
+                kind="line", ax=ax, label="$\\Delta(\\mathrm{ttl})$", color="red"
+            )
             ax.set_ylabel("$\\Delta(\\mathrm{ttl})")
             ax.set_title(f"TTL changes")
 
-
         return fig
 
-    def score(self, rtt, ttl, destination_reached) -> tuple[bool, float, float, float, float, float]:
-        rtt_is_outlier = rtt > self.mu + 3*self.sigma or rtt < self.mu - 3*self.sigma
-        rtt_prob = self.pdf(rtt) 
+    def score(
+        self, rtt, ttl, destination_reached
+    ) -> tuple[bool, float, float, float, float, float]:
+        rtt_is_outlier = (
+            rtt > self.mu + 3 * self.sigma or rtt < self.mu - 3 * self.sigma
+        )
+        rtt_prob = self.pdf(rtt)
         rtt_mu_diff = rtt - self.mu
         success_prob = self.success_prob
         success_score = destination_reached - self.success_prob
-        rtt_ttl_rate = rtt/ttl
-        return rtt_is_outlier, rtt_prob, rtt_mu_diff, success_prob, success_score, rtt_ttl_rate
-    
-    def pdf(self, x)->float:
-        return np.exp(-0.5*((x-self.mu)/self.sigma)**2)/(self.sigma*np.sqrt(2*np.pi))
-    
+        rtt_ttl_rate = rtt / ttl
+        return (
+            rtt_is_outlier,
+            rtt_prob,
+            rtt_mu_diff,
+            success_prob,
+            success_score,
+            rtt_ttl_rate,
+        )
+
+    def pdf(self, x) -> float:
+        return np.exp(-0.5 * ((x - self.mu) / self.sigma) ** 2) / (
+            self.sigma * np.sqrt(2 * np.pi)
+        )
+
     @property
     def timestamp(self):
         return self.tss[-1] if self.tss else None
@@ -212,7 +249,7 @@ class Model:
     @property
     def alpha(self):
         return self.alphas[-1]
-    
+
     @alpha.setter
     def alpha(self, alpha):
         self.alphas.append(alpha)
@@ -220,7 +257,7 @@ class Model:
     @property
     def beta(self):
         return self.betas[-1]
-    
+
     @beta.setter
     def beta(self, beta):
         self.betas.append(beta)
@@ -228,7 +265,7 @@ class Model:
     @property
     def mu(self):
         return self.mus[-1]
-    
+
     @mu.setter
     def mu(self, mu):
         self.mus.append(mu)
@@ -236,15 +273,15 @@ class Model:
     @property
     def sigma(self):
         return self.sigmas[-1]
-    
+
     @sigma.setter
     def sigma(self, sigma):
         self.sigmas.append(sigma)
-    
+
     @property
     def delta_rtt(self):
         return self.delta_rtts[-1] if self.delta_rtts else None
-    
+
     @delta_rtt.setter
     def delta_rtt(self, delta_rtt):
         self.delta_rtts.append(delta_rtt)
@@ -252,7 +289,7 @@ class Model:
     @property
     def delta_ttl(self):
         return self.delta_ttls[-1] if self.delta_ttls else None
-    
+
     @delta_ttl.setter
     def delta_ttl(self, delta_ttl):
         self.delta_ttls.append(delta_ttl)
@@ -260,20 +297,20 @@ class Model:
     @property
     def finished(self):
         return self.successes[-1], self.failures[-1]
-    
+
     @finished.setter
     def finished(self, success):
         a, b = self.finished
-        a, b  = a + 1 if success else a, b + 1 if not success else b
+        a, b = a + 1 if success else a, b + 1 if not success else b
         self.successes.append(a)
         self.failures.append(b)
         self.success_prob = a / (a + b)
-        self.success_var = (a * b) / ((a + b)**2 * (a + b + 1))
+        self.success_var = (a * b) / ((a + b) ** 2 * (a + b + 1))
 
     @property
     def success_prob(self):
         return self.success_probs[-1]
-    
+
     @success_prob.setter
     def success_prob(self, success_prob):
         self.success_probs.append(success_prob)
@@ -281,7 +318,7 @@ class Model:
     @property
     def success_var(self):
         return self.success_vars[-1]
-    
+
     @success_var.setter
     def success_var(self, success_var):
         self.success_vars.append(success_var)
@@ -289,9 +326,7 @@ class Model:
     @property
     def n(self):
         return self.ns[-1]
-    
+
     @n.setter
     def n(self, n):
         self.ns.append(n)
-
-    
