bea9d002b2536a8e0e237d4153c2cf56369a9325
Jan Peřina <perinja2@gmail.com>
2023-08-05 19:02:34 +0200

    change pipeline

--- traced/__init__.py
+++ traced/__init__.py
@@ -4,3 +4,6 @@ from traced.models.bernoulli_model import BernoulliModel
 from traced.models.multinomial_model import MultinomialModel
 from traced.models.normal_model import NormalModel
 from traced.trace_monitor import SiteTraceMonitor, TraceMonitor
+
+
+__ALL__ = [TraceAnalyzer, BaseModel, BernoulliModel, MultinomialModel, NormalModel, SiteTraceMonitor, TraceMonitor]
